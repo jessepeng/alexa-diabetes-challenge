8a6196fa29936309ba753c6a9f30d68f8bf61af1
Jan-Christopher Pien <101@janchristopherpien.de>
2017-06-10 18:35:58 +0200

    update

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
-git pull
-mvn assembly:assembly -DdescriptorId=jar-with-dependencies package
+sudo -H -u jessepeng git pull
+sudo -H -u jessepeng mvn assembly:assembly -DdescriptorId=jar-with-dependencies package
 mv target/alexa-dosage-calculator.war /var/lib/tomcat7/webapps/alexa-dosage-calculator.war
